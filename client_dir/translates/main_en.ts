2f560d48a499c5a6bfbcb3cbde1705f7c5c02aa8
Erepb-89 <erepb-89@yandex,ru>
2025-07-09 08:20:17 +0300

    Methods of class ClientMainWindow changed for using ServerStorage getters. Added create_server, lang_choice, changeEvent methods to ClientMainWindow. Added buttons and labels for starting server, changing language and checking active servers, on Ui_MainWindow (client_main_form.py).

---
+++ client_dir/translates/main_en.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../forms/client_main_form.py" line="425"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="440"/>
+        <source>slot_1</source>
+        <translation>slot_1</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="437"/>
+        <source>slot_2</source>
+        <translation>slot_2</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="441"/>
+        <source>slot_3</source>
+        <translation>slot_3</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="442"/>
+        <source>slot_4</source>
+        <translation>slot_4</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="439"/>
+        <source>slot_5</source>
+        <translation>slot_5</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="438"/>
+        <source>slot_6</source>
+        <translation>slot_6</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="432"/>
+        <source>Столица</source>
+        <translation>Capital</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="433"/>
+        <source>Выбор фракции</source>
+        <translation>Choose faction</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="434"/>
+        <source>currentFaction</source>
+        <translation>currentFaction</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="435"/>
+        <source>capital</source>
+        <translation>capital</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="436"/>
+        <source>Кампания</source>
+        <translation>Campaign</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="443"/>
+        <source>Сравнить две армии</source>
+        <translation>Compare two armies</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="444"/>
+        <source>Добавить игрока</source>
+        <translation>Add player</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="445"/>
+        <source>Имя</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="446"/>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="447"/>
+        <source>Darkest</source>
+        <translation>Darkest</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="448"/>
+        <source>Удалить</source>
+        <translation>Delete</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="449"/>
+        <source>Выбрать игрока</source>
+        <translation>Choose player</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="459"/>
+        <source>1</source>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="460"/>
+        <source>2</source>
+        <translation>2</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="461"/>
+        <source>4</source>
+        <translation>4</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="458"/>
+        <source>3</source>
+        <translation>3</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="456"/>
+        <source>6</source>
+        <translation>6</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="457"/>
+        <source>5</source>
+        <translation>5</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="465"/>
+        <source>Уволить</source>
+        <translation>Fire</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="464"/>
+        <source>Нанять</source>
+        <translation>Hire</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="466"/>
+        <source>Сложность</source>
+        <translation>Difficulty</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="467"/>
+        <source>Создать сервер</source>
+        <translation>Create server</translation>
+    </message>
+    <message>
+        <location filename="../forms/client_main_form.py" line="496"/>
+        <source>Язык</source>
+        <translation>Language</translation>
+    </message>
+</context>
+</TS>
